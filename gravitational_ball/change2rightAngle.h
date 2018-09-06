62281d9f37f97745ffdd4c7a08c2d04c5052eff3
blue Iris <42560690+blue1ris@users.noreply.github.com>
2018-09-07 00:14:06 +0800

    niceDemoVerion

--- gravitational_ball/change2rightAngle.h
+++ gravitational_ball/change2rightAngle.h
@@ -1,2 +1,2 @@
-#pragma once
+   #pragma once
 void change2rightAngle(double c_x,double c_y,double distance,double angle,double *x,double *y);
--- gravitational_ball/startUp.cpp
+++ gravitational_ball/startUp.cpp
@@ -2,31 +2,35 @@
 #include <graphics.h>
 #include <conio.h>
 #include <windows.h>
+#include <math.h>
 #include "change2rightAngle.h"
 #define normalball_number  10
 #define superball_number  10
 
-double fin_main_radius = 8;
-double fin_cent_radius = 3;
-double ratio = 400;//缩放比例
-double a = 0.07;//加速度
-double circleVelocity = 0.7;
-double maxDistance = 300;
+
+double ratio = 250;//缩放比例
+double angleVelocity = 1.5;//两球之间角增加量
+double minDistance = 400;//最近边界（开始缩放边界）
+double maxDistance = 400;//最远边界
 double program_x = 600;
-double program_y = 600;
-typedef struct centralBall{
+double program_y = 600;//屏幕大小
+typedef struct centPoint{
 	double x;
 	double y;
+	double angle;
 	double radius;
-}CENTRALBALL;
-typedef struct mainBall{
+}CENTPOINT;//中心圆点
+typedef struct Ball{
+	double acceleration;
 	double distance;
 	double angle;
 	double x;
 	double y;
 	double radius;
+	double final_radius;
 	double velocity;
-}MAINBALL;
+}BALL;//中心球，操作球
+
 typedef struct normalBall{
 	double distance;
 	double angle;
@@ -34,89 +38,101 @@ typedef struct normalBall{
 	double y;
 	double radius;
 	int kind;
-}NORMALBALL;
+}NORMALBALL;//障碍球，道具
 
-CENTRALBALL centball;
-MAINBALL mainball;
+BALL centball;
+BALL mainball;
+CENTPOINT centpoint;
 NORMALBALL normalball[normalball_number];
 NORMALBALL superball[superball_number];
 
 void defaultData(){
-	centball.x = program_x/2;
-	centball.y = program_y/2;
-	centball.radius = 30;
-	mainball.distance = maxDistance;
+	centpoint.angle = 0;
+	centpoint.radius = 24;
+	centball.angle = 180;
+	centball.distance = 250;
+	centball.final_radius = 20;
+	centball.velocity = 0;
+	centball.acceleration = 0.12;
 	mainball.angle = 0;
-	mainball.radius = 10;
+	mainball.distance = 200;
+	mainball.final_radius = 4;
 	mainball.velocity = 0;
-	IMAGE bkgrd_img;
-	IMAGE sun_img;
-	IMAGE r_sun_img;
-	change2rightAngle(centball.x,centball.y,mainball.distance,mainball.angle,&mainball.x,&mainball.y);//初始化值
-	for(int i = 0;i < normalball_number;i++){
-		normalball[i].distance = 70;
-		normalball[i].angle = 36 * i;
-		normalball[i].radius = 2;
-		change2rightAngle(centball.x,centball.y,normalball[i].distance,normalball[i].angle,&normalball[i].x,&normalball[i].y);
-	}
-	initgraph(program_x,program_y);
-	//setbkcolor(WHITE);
-	//cleardevice();
-	loadimage(&bkgrd_img,_T("./img/background.jpg"));
-	loadimage(&sun_img,_T("./img/sun.jpeg"));
-	loadimage(&r_sun_img,_T("./img/sunAfter.jpg"));
-	//putimage(0,0,&bkgrd_img);
-	//putimage(0,0,&r_sun_img, NOTSRCERASE); 
-	//putimage(0,0,&sun_img);
+	mainball.acceleration = 0.07;
+	
 }
 void logicFunction(){
-	//if(centball.radius>fin_cent_radius) centball.radius =fin_cent_radius * ratio / mainball.distance;
-	if(mainball.radius>fin_main_radius) mainball.radius =fin_main_radius * ratio / mainball.distance;
-	for(int i = 0;i < normalball_number;i++){
-		normalball[i].radius = 4 * ratio / mainball.distance;
-	}
-	change2rightAngle(centball.x,centball.y,mainball.distance,mainball.angle,&mainball.x,&mainball.y);
-	if(mainball.angle==360) mainball.angle = 0;
-	if(mainball.distance < mainball.radius + centball.radius ) mainball.velocity = -mainball.velocity -a;
-	if(mainball.velocity == 0) mainball.velocity = a;
 	
-	if(kbhit()){
-		char ch = getch();
+	centball.radius = centball.final_radius * ratio / (mainball.distance + centball.distance);
+	mainball.radius = mainball.final_radius * ratio / (mainball.distance + centball.distance);
+	
+	change2rightAngle(program_x/2,program_y/2,centpoint.radius,centpoint.angle,&centpoint.x,&centpoint.y);
+	change2rightAngle(centpoint.x,centpoint.y,mainball.distance,mainball.angle,&mainball.x,&mainball.y);
+	change2rightAngle(centpoint.x,centpoint.y,centball.distance,centball.angle,&centball.x,&centball.y);
+
+	for(int i = 0;i < normalball_number ;i++){
+		normalball[i].angle = 36*i;
+		normalball[i].distance = 50 * ratio / (mainball.distance + centball.distance);;
+		normalball[i].radius = 4 * ratio / (mainball.distance + centball.distance);
+		change2rightAngle(centball.x,centball.y,normalball[i].distance,normalball[i].angle,&normalball[i].x,&normalball[i].y);
+	}
+	
+	if(centball.distance + mainball.distance < centball.radius + mainball.radius){
+		centball.velocity = -centball.velocity;
+		mainball.velocity = -mainball.velocity;
+	}
+	if(centball.distance + mainball.distance > maxDistance){
+		centball.velocity = centball.acceleration;
+		mainball.velocity = mainball.acceleration; 
+	}
+	if(_kbhit()){
+		char ch = _getch();
 		if(ch == ' '){
-				mainball.angle += circleVelocity;	
+				mainball.angle += angleVelocity;
+				centball.angle += angleVelocity;
+				if(centball.distance + mainball.distance > minDistance) centpoint.angle += angleVelocity;
 		}		
 	}
-	//centball.x -= 0.1;
-	//centball.y += 0.2;
-	mainball.distance -= mainball.velocity;
-	mainball.velocity += a;
+	centball.distance -= centball.velocity;
+	centball.velocity += centball.acceleration;
+	//if(centball.distance + mainball.distance > minDistance){
+		mainball.distance -= mainball.velocity;
+		mainball.velocity += mainball.acceleration;
+	//}
+	
 }
 
 void printAll(){
 	cleardevice();
+	//for(int i = 0;i < normalball_number;i++){
+	//	circle(normalball[i].x,normalball[i].y,normalball[i].radius);
+	//}
+	setfillcolor(WHITE);
 	for(int i = 0;i < normalball_number;i++){
-		circle(normalball[i].x,normalball[i].y,normalball[i].radius);
+		fillcircle(normalball[i].x,normalball[i].y,normalball[i].radius);
 	}
-	circle(centball.x, centball.y,centball.radius);
-	circle(mainball.x,mainball.y,mainball.radius); 
+	fillcircle(centball.x, centball.y,centball.radius);
+	fillcircle(mainball.x,mainball.y,mainball.radius);
 	line(centball.x,centball.y,mainball.x,mainball.y);
 	setlinecolor(WHITE);
 	
-	Sleep(20);
+	Sleep(15);
 	
-	logicFunction();
+	
 }
 
 void start_up(){
 	defaultData();
-	getch();
+	initgraph(program_x,program_y);
+	_getch();
 	BeginBatchDraw();
 	while(1){
+		logicFunction();
 		printAll();
 		FlushBatchDraw();
 	}
 	EndBatchDraw();
-	getch();
+	_getch();
 	closegraph();
 
 }
